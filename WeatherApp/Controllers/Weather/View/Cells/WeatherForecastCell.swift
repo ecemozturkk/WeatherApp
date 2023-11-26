2d06e6d5975945eaad000bfb4e0e830cca85be96
Ecem Ozturk <ecemozturk704@gmail.com>
2023-11-26 19:43:46 +0300

    added WeatherVC, assets, Utility

---
+++ WeatherApp/Controllers/Weather/View/Cells/WeatherForecastCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherForecastCell.swift
+//  WeatherApp
+//
+//  Created by Ecem Öztürk on 26.11.2023.
+//
+
+import UIKit
+
+class WeatherForecastCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var imgWeatherStatusPic: UIImageView!
+    @IBOutlet weak var lblTemprature: UILabel!
+}
