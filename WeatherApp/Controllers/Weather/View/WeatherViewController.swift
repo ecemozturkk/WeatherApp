28394867260bbbb35378adafd9f4c449b4915751
Ecem Ozturk <ecemozturk704@gmail.com>
2023-11-26 17:03:25 +0300

    added IBOutlets

--- WeatherApp/Controllers/Weather/View/WeatherViewController.swift
+++ WeatherApp/Controllers/Weather/View/WeatherViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
+    
+    //MARK: - IBOutlet(s)
+    
+    @IBOutlet weak var lblDate: UILabel?
+    @IBOutlet weak var lblCity: UILabel?
+    @IBOutlet weak var lblUsername: UILabel?
+    @IBOutlet weak var profileImage: UIImageView?
+    @IBOutlet weak var lblWeatherDescription: UILabel?
+    @IBOutlet weak var imgWeatherStatusPic: UIImageView?
+    
+    @IBOutlet weak var btnForecastSegment: UISegmentedControl?
+    @IBOutlet weak var weatherCollectionViewList: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnSegmentAction(_ sender: UISegmentedControl) {
+    }
+    
 }
+
+
+
 
